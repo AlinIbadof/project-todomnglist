942bb385880ed547f57206ce03a7b6aba2d5d9cc
Alin Ibadof <alinibadof@gmail.com>
2022-12-04 17:59:22 +0200

    refactor todos component to be more structured by using a class

--- frontend/todo/src/app/list-todo/list-todo.component.ts
+++ frontend/todo/src/app/list-todo/list-todo.component.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 
+export class Todo {
+  constructor(
+    public id: number,
+    public description: string,
+    public done: boolean,
+    public targetDate: Date
+  ) {}
+}
+
 @Component({
   selector: 'app-list-todo',
   templateUrl: './list-todo.component.html',
@@ -8,17 +17,20 @@ import { Component } from '@angular/core';
 export class ListTodoComponent {
   //creating a simple to do object
   todos = [
-    {
-      id: 1,
-      description: 'Learn to dance.',
-    },
-    {
-      id: 2,
-      description: 'Become expert at programming.',
-    },
-    {
-      id: 3,
-      description: 'Visit the moon.',
-    },
+    new Todo(1, 'Learn to dance.', false, new Date()),
+    new Todo(2, 'Become expert at programming.', false, new Date()),
+    new Todo(3, 'Visit the moon.', false, new Date()),
+    // {
+    //   id: 1,
+    //   description: 'Learn to dance.',
+    // },
+    // {
+    //   id: 2,
+    //   description: 'Become expert at programming.',
+    // },
+    // {
+    //   id: 3,
+    //   description: 'Visit the moon.',
+    // },
   ];
 }
