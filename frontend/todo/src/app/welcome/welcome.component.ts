c22e201004fabf9b1dbfefd329a3cab69d970d47
Alin Ibadof <alinibadof@gmail.com>
2023-01-02 18:02:49 +0200

    add data service to connect to back-end and receive a response

--- frontend/todo/src/app/welcome/welcome.component.ts
+++ frontend/todo/src/app/welcome/welcome.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { WelcomeDataService } from '../service/data/welcome-data.service';
 
 @Component({
   selector: 'app-welcome',
@@ -7,12 +8,26 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./welcome.component.css'],
 })
 export class WelcomeComponent implements OnInit {
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private service: WelcomeDataService
+  ) {}
 
   name = '';
+  welcomeMessageFromService: string = '';
 
   ngOnInit() {
     this.name = this.route.snapshot.params['name'];
   }
   message = 'Some welcome message';
+
+  getWelcomeMessage() {
+    this.service
+      .executeHelloWorldBeanService()
+      .subscribe((response) => this.handleSucessfulResponse(response));
+  }
+
+  handleSucessfulResponse(response: any) {
+    this.welcomeMessageFromService = response.message;
+  }
 }
